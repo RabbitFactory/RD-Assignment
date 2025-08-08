0750b8a1ff8637c11efb2eca1de7ca842d814e37
Rabbit Factory <jv16th.raj@gmail.com>
2025-08-09 05:59:39 +0600

    basic structure done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReviewForm from './Components/ReviewForm'
+import ReviewCard from './Components/ReviewCard'
+
+const App = () => {
+  return (
+    <div>
+      <ReviewForm></ReviewForm>
+      <ReviewCard></ReviewCard>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ReviewForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const ReviewForm = () => {
+    const [form, setForm] = useState({})
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ReviewForm
