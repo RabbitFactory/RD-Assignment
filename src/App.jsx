66c7f5cd82ee56e6789b84612d1c885baada8f93
Rabbit Factory <jv16th.raj@gmail.com>
2025-08-10 22:55:38 +0600

    localstorage error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import ReviewForm from "./Components/ReviewForm";
 import ReviewList from "./Components/ReviewList";
 
 export default function App() {
-  const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState(() => {
+  return JSON.parse(localStorage.getItem("reviews")) || [];
+});
 
-  useEffect(() => {
-    const stored = JSON.parse(localStorage.getItem("reviews")) || [];
-    setReviews(stored);
-  }, []);
 
   useEffect(() => {
     localStorage.setItem("reviews", JSON.stringify(reviews));
--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
 
 const ReviewCard = ({ shopName, review, rating, date }) => {
 
@@ -6,8 +7,14 @@ const ReviewCard = ({ shopName, review, rating, date }) => {
     <div className="bg-white shadow rounded p-4 mb-4">
       <div className="flex items-center justify-between mb-2">
         <h3 className="text-lg font-bold">{shopName}</h3>
-        <div className="text-yellow-500">
-          {"★".repeat(rating) + "☆".repeat(5 - rating)}
+        <div className="">
+          
+          <Rating
+            style={{ maxWidth: 120 }}
+            value={rating}
+            isRequired
+            readOnly
+          />
         </div>
       </div>
       <p className="text-gray-700 mb-2">{review}</p>
